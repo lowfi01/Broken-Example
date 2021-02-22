4740708ee64064fefb73cf40c2f6eb7a497eec1b
JAMES HERRERO <100859697@student.swin.edu.au>
2021-02-22 14:46:32 +1100

    Add Activity interface to client

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 import './styles.css';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then((response) => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
       setActivities(response.data);
     });
   }, [])
@@ -16,7 +17,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Activities'/>
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
             <List.Item key={activity.id}>
               <p>{activity.title}</p>
             </List.Item>
